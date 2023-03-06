2ecf3d3fde2bcadecbf85b6b5fda63ce0b31dd55
Boris Demin <demin.b.d@gmail.com>
2023-03-06 23:20:01 +0300

    Completed stage 2

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "4896");
+        return Integer.valueOf(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        addRoutes(app);
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+}
