35a278784c7965a2fb195f76353ecee97b03f91c
Boris Demin <demin.b.d@gmail.com>
2023-03-09 17:00:37 +0300

    step four is Done

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,14 @@
 package hexlet.code;
 
+import hexlet.code.controllers.RootController;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+
 public class App {
 
     private static int getPort() {
@@ -9,14 +17,35 @@ public class App {
     }
 
     private static void addRoutes(Javalin app) {
-        app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/", RootController.helloWorld);
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+        ClassLoaderTemplateResolver classLoaderTemplateResolver = new ClassLoaderTemplateResolver();
+        classLoaderTemplateResolver.setPrefix("/templates/");
+        templateEngine.addTemplateResolver(classLoaderTemplateResolver);
+
+        return templateEngine;
     }
     public static Javalin getApp() {
+
         Javalin app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
+            JavalinThymeleaf.init(getTemplateEngine());
         });
 
+        // Добавляем маршруты в приложение
         addRoutes(app);
+
+        // Обработчик before запускается перед каждым запросом
+        // Устанавливаем атрибут ctx для запросов
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
 
         return app;
     }
--- app/src/main/java/hexlet/code/controllers/RootController.java
+++ app/src/main/java/hexlet/code/controllers/RootController.java
@@ -4,7 +4,7 @@ import io.javalin.http.Handler;
 
 public final class RootController {
 
-//    public static Handler welcome = ctx -> {
-//        ctx.
-//    }
+    public static Handler helloWorld = ctx -> {
+        ctx.render("index.html");
+    };
 }
